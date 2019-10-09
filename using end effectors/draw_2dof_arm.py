8ec119ec75fde9fda2bb35aa18a88ec8a6811670
shrey23812 <40837035+shrey23812@users.noreply.github.com>
2019-10-08 21:05:57 -0500

    Add files via upload

---
+++ using end effectors/draw_2dof_arm.py
@@ -0,0 +1,61 @@
+import pygame
+import math
+import numpy as np
+import random
+
+
+class ARM2D(object):
+	def __init__ (self):
+		# Initialize pygame
+		pygame.init()
+		self.SCREEN_WIDTH = 500
+		self.SCREEN_HEIGHT = 250
+		
+		self.FPS = 15
+		self.l1 = 100
+		self.l2 = 100
+				
+		self.arm_thickness = 5
+		self.origin = [self.SCREEN_WIDTH//2 , self.SCREEN_HEIGHT-self.arm_thickness]#-self.arm_thickness
+		
+		self.color_black = [0,0,0]
+		self.color_white = [255,255,255]
+		
+	def reset(self):
+		# Angle in radian
+		self.screen = pygame.display.set_mode([self.SCREEN_WIDTH, self.SCREEN_HEIGHT])
+		self.clock = pygame.time.Clock()
+
+	def step(self, angle1, angle2):
+		pygame.event.pump()
+		self.screen.fill(self.color_black)
+		# Set arm angle
+		self.angle1 = angle1 * math.pi/180.0
+		self.angle2 = angle2 * math.pi/180.0
+
+		self.arm_1_point_list = [self.origin, [self.origin[0] + self.l1*math.cos(self.angle1), self.origin[1] - self.l1*math.sin(self.angle1)]]
+		self.arm_2_point_list = [[self.origin[0] + self.l1*math.cos(self.angle1), self.origin[1] - self.l1*math.sin(self.angle1)],[self.arm_1_point_list[1][0]+self.l2*math.cos(self.angle1+self.angle2),self.arm_1_point_list[1][1]-self.l2*math.sin(self.angle1+self.angle2)]]
+		pygame.draw.lines(self.screen, self.color_white, False, self.arm_1_point_list, self.arm_thickness)
+		pygame.draw.lines(self.screen, self.color_white, False, self.arm_2_point_list, self.arm_thickness)
+
+		# Save screenshot
+		pygame.image.save(self.screen, "./images/" + "[{0:.2f},{0:.2f}]".format(self.arm_2_point_list[1][0],self.arm_2_point_list[1][1]) + ".jpg")
+		# Print angle
+		# print ("Arm Angle : ", angle1)
+
+		print("end_effector_point: ",self.arm_2_point_list[1])
+
+		self.clock.tick(self.FPS)
+		pygame.display.flip()
+
+
+
+if __name__ == "__main__":   
+	game = ARM2D()
+	game.reset()
+
+	for i in range(90):
+		for j in range(90):
+			game.step(i,j)
+
+	
---
+++ using end effectors/train.py
@@ -0,0 +1,126 @@
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Dense, Dropout, Conv2D,  Cropping2D
+from keras.models import load_model
+from keras.optimizers import Adam
+import cv2
+import numpy as np
+from os import listdir, path
+import matplotlib.pyplot as plt
+
+IMAGE_WIDTH = 500
+IMAGE_HEIGHT = 250
+IMAGE_CHANNELS = 3
+
+# initialize parameters
+DATA_DIR = "./images/"
+
+BATCH_SIZE = 32
+NUM_EPOCHS = 40
+
+images = []
+measurements = []
+
+# Load the images
+for f in listdir(DATA_DIR):
+	images.append(cv2.imread(DATA_DIR + f))
+	var = f[1:-5].split(",")
+	measurements.append([float(var[0]),float(var[1])])
+
+# Define numpy arrays
+X_train = np.array(images)
+y_train = np.array(measurements)
+
+# Print out the shapes of the training data 
+print(X_train.shape)
+print(y_train.shape)
+
+#shuffle X_train and y_train for validation split
+s = np.arange(X_train.shape[0])
+np.random.shuffle(s)
+
+X_train = X_train[s]
+y_train = y_train[s]
+
+# build the model
+model = Sequential()
+
+filename = "model.h5"
+
+if (not path.exists(filename)):
+	print ("Creating new model")
+	# Normalize image
+	model.add(Lambda( lambda x: x/127.5 - 1.0 , input_shape=(IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)))
+
+	model.add( Conv2D( 24, 5, 5, subsample=(2,2), activation = 'relu' ) )
+	model.add( Conv2D( 36, 5, 5, subsample=(2,2), activation = 'relu' ) )
+	model.add( Conv2D( 48, 5, 5, subsample=(2,2), activation = 'relu' ) )
+	model.add( Conv2D( 64, 3, 3, subsample=(1,1), activation = 'relu' ) )
+	model.add( Conv2D( 96, 3, 3, subsample=(1,1), activation = 'relu' ) )
+	model.add( Conv2D( 128, 3, 3, subsample=(1,1), activation = 'relu' ) )
+	model.add( Flatten() )
+	# model.add(Dropout(0.8))
+	model.add( Dense( 100, activation = 'relu' ) )
+	model.add( Dense( 50, activation = 'relu') )
+	model.add( Dense( 10, activation = 'relu' ) )
+	model.add( Dense( 2 ) )
+
+	# model.add(Lambda( lambda x: x/127.5 - 1.0 , input_shape=(IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)))
+	# model.add( Conv2D( 24, 5, 5, subsample=(2,2), activation = 'relu' ) )
+	# model.add( Conv2D( 36, 5, 5, subsample=(2,2), activation = 'relu' ) )
+	# model.add( Conv2D( 48, 5, 5, subsample=(2,2), activation = 'relu' ) )
+	# model.add( Conv2D( 64, 3, 3, subsample=(1,1), activation = 'relu' ) )
+	# model.add( Conv2D( 96, 3, 3, subsample=(1,1), activation = 'relu' ) )
+	# model.add( Conv2D( 128, 3, 3, subsample=(1,1), activation = 'relu' ) )
+	# model.add( Flatten() )
+	# # model.add(Dropout(0.8))
+	# model.add( Dense( 100, activation = 'relu' ) )
+	# model.add( Dense( 50, activation = 'relu') )
+	# model.add( Dense( 10, activation = 'relu' ) )
+	# model.add( Dense( 1 ) )
+	
+	# Print Model Summary
+	#model.summary()
+
+	# Compile Model
+	model.compile(optimizer=Adam(lr=1e-3), loss="mse", metrics=['accuracy'])
+	model.fit(X_train, y_train, shuffle= True, epochs=NUM_EPOCHS, batch_size=BATCH_SIZE,validation_split = 0.1)
+	#save the trained model as h5 file
+	print("Saving model")
+	model.save(filename, overwrite=True)
+	
+else:
+	print ("Using Existing Model")
+	model = load_model(filename)
+
+# Run predictions
+print("Running prediction")
+x=[]
+y=[]
+i = 0
+for f in listdir(DATA_DIR):
+	img = cv2.imread(DATA_DIR + f)
+	img = img.reshape(1, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)
+	img = np.array(img)
+
+	var = f[1:-5].split(",")
+	print(var, " : ", model.predict(img)[0],i,np.linalg.norm(np.array(var,dtype="float32")-model.predict(img)[0]))#,abs(np.array(var,dtype="float32")-np.array(model.predict(img)[0])))
+	x.append(np.linalg.norm(np.array(var,dtype="float32")-model.predict(img)[0]))
+	y.append(i)
+	i = i+1
+error = np.sum(x)/len(x)
+print("Error:",error)
+plt.plot(x,y)
+plt.show()
+
+# for i in range(180):
+# 	img = cv2.imread('./images/' + str(i) + '.jpg')
+# 	img = img.reshape(1, IMAGE_HEIGHT, IMAGE_WIDTH, IMAGE_CHANNELS)
+# 	img = np.array(img)
+
+# 	print(i, " : ", model.predict(img)[0])
+# 	x.append(i)
+# 	y.append(np.abs(i-model.predict(img)[0]))
+# fig, ax = plt.subplots()
+# ax.plot(x, y)
+# # fig.savefig("360.png")
+# plt.show()
